9293448943610b4926caf8ebf0d5d71148639467
Aditya Prasetya Nugraha <58680942+adit75664@users.noreply.github.com>
2025-10-25 08:10:15 +0700

    menambah validateForm di JS pada message-section

--- CodingCamp-20Oct2025-aditya/js/script.js
+++ CodingCamp-20Oct2025-aditya/js/script.js
@@ -7,3 +7,20 @@ function welcomeSpeech() {
     // Greet the user with their name
     document.getElementById('greet-name').innerHTML = `Hi ${name}, `;
 }
+
+
+function validateForm() {
+    //get form input value
+    const name = document.getElementById('name-input').value;
+    const email = document.getElementById('email-input').value;
+    const message = document.getElementById('message-input').value;
+
+    //check if any field is empty
+    if (name === "" || email === "" || message === "") {
+        //show alert if any field empty
+        alert("All fields are required!");
+    } else {
+        //show succes message
+        alert(`Thank you ${name}, your message has been submitted successfully!`);
+    }
+}
