766de5a081298c445dcd18d696c495ea2a9936db
Aditya Prasetya Nugraha <58680942+adit75664@users.noreply.github.com>
2025-10-25 07:19:38 +0700

    menambah welcomeSpeech di JS pada home-section

--- CodingCamp-20Oct2025-aditya/js/script.js
+++ CodingCamp-20Oct2025-aditya/js/script.js
@@ -1,3 +1,9 @@
-console.log('hello world');
+welcomeSpeech();
 
+function welcomeSpeech() {
+    // Show prompt to ask for user's name
+    let name = prompt("Enter your name:");
 
+    // Greet the user with their name
+    document.getElementById('greet-name').innerHTML = `Hi ${name}, `;
+}
